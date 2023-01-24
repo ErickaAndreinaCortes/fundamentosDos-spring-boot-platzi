c9afa61338fbc1e61b70f757bcb99f6129813919
Ericka <ericka.cortescastillo@nttdata.com>
2023-01-24 15:22:09 -0300

    firts commit

---
+++ src/main/java/com/fundamentosDosPlatziSpringBoot/fundamentosDos/FundamentosDosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosDosPlatziSpringBoot.fundamentosDos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosDosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosDosPlatziSpringBoot/fundamentosDos/FundamentosDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosDosPlatziSpringBoot.fundamentosDos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
